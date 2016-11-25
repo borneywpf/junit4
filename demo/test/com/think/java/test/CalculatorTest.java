fa329e8eeb7a35ad2a0d276a0cc55da8e1db3ac1
borney <borneywpf@gmail.com>
2016-11-25 18:27:35 +0800

    add demo

---
+++ demo/test/com/think/java/test/CalculatorTest.java
@@ -0,0 +1,24 @@
+package com.think.java.test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+/**
+ * Created by borney on 11/25/16.
+ */
+public class CalculatorTest {
+
+    @Test
+    public void evaluatesExpression() {
+        Calculator calculator = new Calculator();
+        assertEquals(6, calculator.evaluate("1+2+3"));
+    }
+
+    @Test
+    public void evaluatesNotEqueasExpression() {
+        Calculator calculator = new Calculator();
+        assertNotEquals(5, calculator.evaluate("1+2+3"));
+    }
+}
