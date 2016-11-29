cbb2534260056cb8ccbe79d4915dfb6372534261
borney <borneywpf@gmail.com>
2016-11-29 18:34:05 +0800

    modify Assumptions

---
+++ demo/src/com/think/junit/User.java
@@ -0,0 +1,14 @@
+package com.think.junit;
+
+/**
+ * Created by borney on 11/29/16.
+ */
+public class User {
+    public User(String optimus) {
+
+    }
+
+    public String configFileName() {
+        return "configfiles\\optimus.cfg";
+    }
+}
--- demo/test/com/think/junit/test/Assumptions.java
+++ demo/test/com/think/junit/test/Assumptions.java
@@ -1,7 +1,22 @@
 package com.think.junit.test;
 
+import com.think.junit.User;
+
+import org.junit.Test;
+
+import java.io.File;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assume.assumeThat;
+
 /**
  * Created by borney on 11/29/16.
  */
 public class Assumptions {
+    @Test public void filenameIncludesUsername() {
+        System.out.println("separatorChar = " + File.separatorChar);
+        assumeThat(String.valueOf(File.separatorChar), is("\\"));
+        assertThat(new User("optimus").configFileName(), is("configfiles\\optimus.cfg"));
+    }
 }
